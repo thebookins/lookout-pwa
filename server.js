56df2bd949a6dc0cf15f79d3405b6350ebfa8b79
Paul Dickens <dickens.paul@gmail.com>
2017-12-31 10:12:23 +1100

    use PathLocationStrategy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // If an incoming request uses
 // a protocol other than HTTPS,
@@ -25,6 +26,11 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default
 // Heroku port
